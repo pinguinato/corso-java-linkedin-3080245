c7e8d5accb358837156e60d4b6a3c7608f658e5c
Roberto Gianotto <gianottoroberto@gmail.com>
2024-12-13 23:09:50 +0000

    metodo getCustomer()

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -2,6 +2,8 @@ package bank;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DataSource {
@@ -21,8 +23,37 @@ public class DataSource {
     return connection;
   }
 
+  public static Customer getCustomer(String username) {
+    String sql = "SELECT * FROM customers WHERE username = ?";
+    Customer customer = null;
+
+    try (Connection connection = connect();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+      preparedStatement.setString(1, username);
+      try (ResultSet resultSet = preparedStatement.executeQuery()) {
+
+        customer = new Customer(
+            resultSet.getInt("id"),
+            resultSet.getString("name"),
+            resultSet.getString("username"),
+            resultSet.getString("password"),
+            resultSet.getInt("account_id"));
+      }
+    } catch (SQLException sqlException) {
+      sqlException.printStackTrace();
+    }
+
+    return customer;
+
+  }
+
   public static void main(String[] args) {
+    // test di connessione
     connect();
+
+    // test di ricerca Customer
+    Customer customer = getCustomer("amcerlaineqm@admin.ch");
+    System.out.println(customer.getName());
   }
 
 }
