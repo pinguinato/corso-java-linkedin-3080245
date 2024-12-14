7a831d923bd66e00d5a245e59fa4e9445ba593d4
Roberto Gianotto <gianottoroberto@gmail.com>
2024-12-14 11:23:56 +0000

    Metodo del prelievo completo

--- src/main/java/bank/Account.java
+++ src/main/java/bank/Account.java
@@ -48,7 +48,14 @@ public class Account {
     }
   }
 
-  public void withdraw(double amount) {
-
+  public void withdraw(double amount) throws AmountException {
+    if (amount < 0) {
+      throw new AmountException("The withdraw amount must be greater than 0.");
+    } else if (amount > getBalance()) {
+      throw new AmountException("You do not have sufficient funds for this withdrawal.");
+    } else {
+      double newBalance = balance - amount;
+      DataSource.updateAccountBalance(id, newBalance);
+    }
   }
 }
